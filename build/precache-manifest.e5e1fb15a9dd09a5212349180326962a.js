15667094de3cfa56751d5ca596ad9ba89eee23a5
maelstrm <aryeh.the.lion@gmail.com>
2020-04-02 09:15:17 -0500

    setup react ts project, add sample chart

---
+++ build/precache-manifest.e5e1fb15a9dd09a5212349180326962a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fc273c1916374ebefcc63e331f696c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "340f2093842d0e803aa5",
+    "url": "/static/css/main.9c2802ae.chunk.css"
+  },
+  {
+    "revision": "4fbe67f30958f01e120c",
+    "url": "/static/js/2.f1a625f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f1a625f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340f2093842d0e803aa5",
+    "url": "/static/js/main.4a246ec8.chunk.js"
+  },
+  {
+    "revision": "d72d08c1af02c20fa5d9",
+    "url": "/static/js/runtime-main.d0cf6abc.js"
+  }
+]);
